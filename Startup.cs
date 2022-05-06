6660169c4b9fad11913acfb14889415e85a3fe48
Chris Cheetham <chris@cheetham.com>
2022-05-06 13:58:23 -0400

    Remove HTTPS redirect (closes #4)

--- Startup.cs
+++ Startup.cs
@@ -40,7 +40,6 @@ namespace Sample
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sample v1"));
             }
 
-            app.UseHttpsRedirection();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
