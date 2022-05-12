408b877986611a70a6ed0646a495d5309ad3220f
Chris Cheetham <chris@cheetham.com>
2022-05-12 12:45:29 -0400

    Port app to net6.0 (closes #6)

--- Program.cs
+++ Program.cs
@@ -1,27 +1,41 @@
-using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Steeltoe.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.OpenApi.Models;
+using Steeltoe.Management.Endpoint;
+using Steeltoe.Management.Tracing;
 
-namespace Sample
+var builder = WebApplication.CreateBuilder(args);
+
+// honor PORT var if set
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+var url = string.Concat("http://0.0.0.0:", port);
+builder.WebHost.UseUrls(url);
+
+// Learn more about management endpoints at https://docs.steeltoe.io/api/v3/management/
+builder.Services.AddAllActuators(builder.Configuration);
+builder.Services.ActivateActuatorEndpoints();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
-            var url = String.Concat("http://0.0.0.0:", port);
-
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>().UseUrls(url);
-                });
-        }
-    }
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseFileServer(new FileServerOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "StaticFiles")),
+    RequestPath = "",
+});
+
+app.MapControllers();
+app.Run();
